2cdbada47d877309b1473b8e28da8b53110ff583
thotasanju2025 <saisanjana.thota@utdallas.edu>
2025-11-04 13:42:18 -0600

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -4,6 +4,22 @@ public class HomePage
   {
     public static void main(String[] args)
     {
-      
+      System.out.println("Welcome to Rythmix - an AI lyrics Generator");
+
+      boolean running = true;
+      while(running)
+        {
+          System.out.println("Please choose an option: ");
+          System.out.println("1. Generate New Lyrics");
+          System.out.println("2. View Saved Lyrics");
+          System.out.println("3. Profile Page");
+          System.out.println("4. Settings");
+          System.out.println("5. Logout");
+          System.out.pring("Enter your choice(1-5): ");
+
+          int choice = sc.nextInt();
+          running = nav.navigation(choice);
+        }
+      sc.close();
     }
   }
