d2a34fa8130e429b380b8205d9e375be50b98c30
thotasanju2025 <saisanjana.thota@utdallas.edu>
2025-11-17 12:04:09 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "rythmix-cache-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+// Install event: cache basic app shell files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+// Activate event: clean up old caches if any
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event: try cache first, then network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
